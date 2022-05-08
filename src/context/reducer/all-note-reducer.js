49772c2ad7c89fefc471b68ff5c6ada662f7597c
m26unkwn <getmonushukla@gmail.com>
2022-05-08 15:28:01 +0530

    default: case updated

--- src/context/reducer/all-note-reducer.js
+++ src/context/reducer/all-note-reducer.js
@@ -1,7 +1,8 @@
 export default function allNoteReducer(state, action) {
   switch (action.type) {
     case "ADD_NOTE_INTO_NOTES":
-      console.log("data into payload", action.payload);
       return { ...state, notes: action.payload };
+    default:
+      return state;
   }
 }
